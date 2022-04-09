2cdd70085c1f70bbfd1d12d010b8c76cf498a3e2
Daniel Romero <croweloper@gmail.com>
2022-04-09 11:50:45 -0500

    Actualizacion NPS

--- BanBif.NPS/Controllers/VotoController.cs
+++ BanBif.NPS/Controllers/VotoController.cs
@@ -2,23 +2,46 @@
 using BanBif.NPS.BL;
 using System.Web.Mvc;
 
+
+
 namespace BanBif.NPS.Controllers
 {
     public class VotoController : Controller
     {
 
-        // GET: Voto
+        //// GET: Voto
+        //[HttpPost]
+        //public ActionResult Index(PollResultData data) 
+        //{
+        //    var pollUserBL = new PollUserBL();
+        //    var response = pollUserBL.SavePollResult(data);
+        //    //var notificacionRequest = new NPS_Notificacion_request { id = 11789 };
+        //    //var responseNotificacion = pollUserBL.EnvioNotificacion(notificacionRequest);
+        //    return Json(new { result = true, data = response, mensaje = "Información guardada con éxito." }, JsonRequestBehavior.AllowGet);
+        //}
+
         [HttpPost]
-        public ActionResult Index(PollResultData data) 
+        public ActionResult Index(NPS_NUEVO_RESULTADO request)
         {
-            var pollUserBL = new PollUserBL();
-            var response = pollUserBL.SavePollResult(data);
-            //var notificacionRequest = new NPS_Notificacion_request { id = 11789 };
-            //var responseNotificacion = pollUserBL.EnvioNotificacion(notificacionRequest);
-            return Json(new { result = true, data = response, mensaje = "Información guardada con éxito." }, JsonRequestBehavior.AllowGet);
+
+            return Json(new { result = true, data = request, mensaje = "Información guardada con éxito." }, JsonRequestBehavior.AllowGet);
         }
 
+        [HttpPost]
+        public ActionResult RegistrarEncuesta(NPS_NUEVO_RESULTADO request)
+        {
+            var oBL = new NPSNuevoRegistro();
+            return Json(oBL.RegistarEncuestaNPS(request));
+        }
 
-   
+        ////[Route("api/NPSNUEVO/RegistarEncuesta")]
+        //[HttpPost]
+        //public IHttpActionResult RegistarEncuesta(NPS_NUEVO_RESULTADO request)
+        //{
+        //    var oBL = new NPSNuevo();
+        //    return Json(oBL.RegistarEncuestaNPSNuevo(request));
+        //}
+
+
     }
 }
